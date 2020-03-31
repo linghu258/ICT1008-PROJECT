a07a7a9495154ef0746db6eba5f21126f2599e34
linghu258 <ranger0504@hotmail.com>
2020-03-31 22:39:21 +0800

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,8 +1,5 @@
-import folium
-import os
-import json
-import csv
 import math
+
 
 from collections import defaultdict
 
@@ -10,31 +7,6 @@ MAX_WALK_RANGE = 0.08  # km
 WALK_SPEED = 5.0  # km
 BUS_SPEED = 30.0  # km
 MRT_SPEED = 80.0  # km
-
-# Code to be called by GUI
-# 33
-"""
-nodes={}
-with open('exportBuilding.geojson') as access_json:
-    read_content = json.load(access_json)
-    feature_access = read_content['features']
-
-    for feature_data in feature_access:
-        buildingName = feature_data['properties']
-        if 'name' in buildingName:
-            retrieveHDB = buildingName['name']
-            nodes[retrieveHDB]= find_midpoint(feature_data['geometry']['coordinates'])
-        elif 'addr:housename' in buildingName and 'addr:housenumber' in buildingName:
-            retrieveName = buildingName['addr:housename'] + " " + buildingName['addr:housenumber']
-            nodes[retrieveName]= find_midpoint(feature_data['geometry']['coordinates'])
-
-pathfinder = Dijkstra(nodes) 
-pathfinder.create_edges()
-graph = pathfinder.build_graph()
-path = pathfinder.find_shortest_path(graph,"The Meadows 641C","The Meadows 641A")
-
-"""
-# 33
 
 
 class Dijkstra:
@@ -77,46 +49,47 @@ class Dijkstra:
                     self.edges.append(edge)
 
     # to be called by gui
-    #builds a graph which contains a dictionary of all the nodes with a list of edges as their values.
+    # builds a graph which contains a dictionary of all the nodes with a list of edges as their values.
     def build_graph(self):
         graph = defaultdict(list)
         seen_edges = defaultdict(int)
 
-        #for every edge in the list of edges
+        # for every edge in the list of edges
         for src, dst, weight, mode in self.edges:
 
             # checking for duplicated edge entries
             seen_edges[(src, dst, weight)] += 1
             if seen_edges[(src, dst, weight)] > 1:  # dont add to graph if duplicated
                 continue
-            graph[src].append([dst, weight, mode]) #add edge to graph
+            graph[src].append([dst, weight, mode])  # add edge to graph
 
         return graph
 
     # function to be called by gui to find path from src to dst, returns a list of coordinates for plotting lines
     def find_shortest_path(self, graph, src, dst):
-        d, prev = dijkstra(graph, src, dst) 
-        path = find_path(prev, [dst, 'walk']) #recieves a list of names of the path traveled
+        d, prev = dijkstra(graph, src, dst)
+        path = find_path(prev, [dst, 'walk'])  # recieves a list of names of the path traveled
         newpath = []
         print(path)
 
-        #for every node in the path , get the coordinates of the node and add it into newpath
-        #swap is used to change the order of long, lat to lat, long
-        for x in path: 
-            if x[0] in self.nodes: #if it is a hdb 
+        # for every node in the path , get the coordinates of the node and add it into newpath
+        # swap is used to change the order of long, lat to lat, long
+        for x in path:
+            if x[0] in self.nodes:  # if it is a hdb
                 newpath.append(swap(self.nodes[x[0]]))
-            elif x[0] in self.busnodes: #if it is a busstop
+            elif x[0] in self.busnodes:  # if it is a busstop
                 newpath.append(swap(self.busnodes[x[0]]))
-            elif x[0] in self.mrtnodes: #if it is mrt nodes
+            elif x[0] in self.mrtnodes:  # if it is mrt nodes
                 newpath.append(swap(self.mrtnodes[x[0]]))
-            elif x[1] == "LRT": #if it is a mrt route
+            elif x[1] == "LRT":  # if it is a mrt route
                 newpath.append(swap(self.mrtroutes[x[0]]))
-            else: #then it is definetly a buspath
+            else:  # then it is definetly a buspath
                 newpath.append(swap(self.busroutes[x[0]]))
-        return newpath 
+        return newpath
+
+    # checks if it is a neighbour then return an edge , [src, dest,weight,modeoftravel]. Else return none
 
 
-# checks if it is a neighbour then return an edge , [src, dest,weight,modeoftravel]. Else return none
 def add_neighbour(k1, v1, k2, v2):
     # If it is within walking distance then there is an edge between the 2 points
     distance = calc_distance(v1, v2)
@@ -124,48 +97,47 @@ def add_neighbour(k1, v1, k2, v2):
         return [k1, k2, distance, 'walk']
 
 
-
 # to be called by gui after calling build graph
-# generate the shortest graph based on the src 
+# generate the shortest graph based on the src
 def dijkstra(graph, src, dst=None):
     nodes = []
     for n in graph:
         nodes.append(n)
         nodes += [x[0] for x in graph[n]]
-    
-    #create a queue of the nodes
-    q = set(nodes)
-    nodes = list(q) 
 
-    #create a dictionary for prev and distance
+    # create a queue of the nodes
+    q = set(nodes)
+    nodes = list(q)
+
+    # create a dictionary for prev and distance
     dist = dict()
     prev = dict()
 
-    #set every node to unvisted (infinity cost)
+    # set every node to unvisted (infinity cost)
     for n in nodes:
         dist[n] = float('inf')
         prev[n] = None
 
-    dist[src] = 0 #initialise starting point as 0 cost
+    dist[src] = 0  # initialise starting point as 0 cost
 
-    #while the queue is not empty
+    # while the queue is not empty
     while q:
-        u = min(q, key=dist.get) #get the lowest cost out of all the edges
-        q.remove(u) #remove the first item
+        u = min(q, key=dist.get)  # get the lowest cost out of all the edges
+        q.remove(u)  # remove the first item
 
-        #if the item removed is the destination then return the distance and all the previous nodes taken
+        # if the item removed is the destination then return the distance and all the previous nodes taken
         if dst is not None and u == dst:
             return dist[dst], prev
 
-        #From the neighbouring node with the lowest cost, get a list of all of its edges
+        # From the neighbouring node with the lowest cost, get a list of all of its edges
         for v, w, mode in graph.get(u, []):
-            
-            #add the weight of the edge into the existing cost
+
+            # add the weight of the edge into the existing cost
             alt = dist[u] + w
 
-            #checks if the cost is lesser then the previously existing cost
+            # checks if the cost is lesser then the previously existing cost
             if alt < dist[v]:
-                dist[v] = alt #new cost
+                dist[v] = alt  # new cost
                 prev[v] = [u, mode]
 
     return dist, prev
@@ -179,9 +151,10 @@ def find_path(pr, node):  # generate path list based on parent points 'prev'
     return p[::-1]
 
 
-#swaps coordinates
+# swaps coordinates
 def swap(coord):
     return [coord[1], coord[0]]
+
 
 # returns the midpoint of the polygon coordinates
 def find_midpoint(L):
@@ -191,20 +164,21 @@ def find_midpoint(L):
         lat.append(l[0])
         long.append(l[1])
 
-    return [sum(lat)/len(lat), sum(long)/len(long)]
+    return [sum(lat) / len(lat), sum(long) / len(long)]
 
 
-# Recieves 2 list/tuple of coordinates, (srclat,srclong)(destLat,destLong), returns the distance between 2 points
+# Receives 2 list/tuple of coordinates, (srclat,srclong)(destLat,destLong), returns the distance between 2 points
+# Calculate distance between two location using coordinates
 def calc_distance(src, dest):
     lat1, lon1 = src
     lat2, lon2 = dest
     radius = 6371  # km
 
-    dlat = math.radians(lat2-lat1)
-    dlon = math.radians(lon2-lon1)
+    dlat = math.radians(lat2 - lat1)
+    dlon = math.radians(lon2 - lon1)
 
-    a = math.sin(dlat/2) * math.sin(dlat/2) + math.cos(math.radians(lat1)) \
-        * math.cos(math.radians(lat2)) * math.sin(dlon/2) * math.sin(dlon/2)
-    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+    a = math.sin(dlat / 2) * math.sin(dlat / 2) + math.cos(math.radians(lat1)) \
+        * math.cos(math.radians(lat2)) * math.sin(dlon / 2) * math.sin(dlon / 2)
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
     d = radius * c
     return d
